076b0b5fa234e61c48b15d91ed401ade357d37f1
horsjaco117 <jacob_horsley@live.com>
2025-01-23 19:06:33 -0700

    Add project files.

---
+++ Simple Calculator/SimpleCalculator.vb
@@ -0,0 +1,25 @@
+﻿'Jacob Horsley
+'Spring 2025
+'RCET 2265
+'Simple Calculator
+'
+
+Option Explicit On
+Option Strict On
+
+
+Module SimpleCalculator
+    Sub Main()
+
+        Console.WriteLine("Choose a Number: 22")
+        Console.WriteLine()
+
+        Console.WriteLine("Choose a Number: 23")
+        Console.WriteLine()
+
+
+
+
+    End Sub
+
+End Module
